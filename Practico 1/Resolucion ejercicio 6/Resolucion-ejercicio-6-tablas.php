5c5db4d30b58c2ed5f38f9370ebe914cc61311e9
Coria97 <santiagocoria@live.com.ar>
2022-08-26 14:32:08 -0300

    ejercicio 6 completo

--- Practico 1/Resolucion ejercicio 6/Resolucion-ejercicio-6-tablas.php
+++ Practico 1/Resolucion ejercicio 6/Resolucion-ejercicio-6-tablas.php
@@ -1,38 +1,42 @@
 <?php 
+    require_once 'template/head.html';
     $i = 1;
     $finish = 0;
     $mult = 0;
     if(isset($_GET['mult']))
         $mult = $_GET['mult'];   
 ?>
-<table>
-    <?php 
-        while($finish <= $mult){
-            if($finish == 0){
-                echo "<tr> ";
-                echo "<td></td>";
-                while($i <= $mult){
-                    echo "<td class = 'color'> $i </td>";
-                    $i = $i + 1;
+<body>
+    <table>
+        <?php 
+            while($finish <= $mult){
+                if($finish == 0){
+                    echo "<tr> ";
+                    echo "<td class = 'style0'></td>";
+                    while($i <= $mult){
+                        echo "<td class = 'style1'> $i </td>";
+                        $i = $i + 1;
+                    }
+                    $i = 1;
+                    echo "</tr>";
+                }
+            if($finish > 0){
+                    echo "<tr> ";
+                    echo "<td class = 'style1'> $finish </td>";
+                    while($i <= $mult){
+                        $result = $i * $finish;
+                        if ($i == $finish)
+                            echo "<td class = 'style2'> $result </td>";
+                        else
+                            echo "<td> $result </td>";
+                        $i = $i + 1;
+                    }
+                    echo "</tr>";
                 }
                 $i = 1;
-                echo "</tr>";
+                $finish = $finish + 1;
             }
-         if($finish > 0){
-                echo "<tr> ";
-                echo "<td class = 'color'> $finish </td>";
-                while($i <= $mult){
-                    $result = $i * $finish;
-                    if ($i == $finish)
-                        echo "<td class = 'color'> $result </td>";
-                    else
-                        echo "<td> $result </td>";
-                    $i = $i + 1;
-                }
-                echo "</tr>";
-            }
-            $i = 1;
-            $finish = $finish + 1;
-        }
-    ?>
-</table>
+        ?>
+    </table>
+</body>
+</html>
--- Practico 1/Resolucion ejercicio 6/Resolucion-ejercicio-6.php
+++ Practico 1/Resolucion ejercicio 6/Resolucion-ejercicio-6.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title> Resolucion ejercicio 6 </title>
-</head>
+<?php
+    require_once 'template/head.html';
+?>
 <body>
     <form id = "formulario-GET" action= "Resolucion-ejercicio-6-tablas.php" method= "GET">
         <label for="mult">Ver las tablas de multiplicar hasta:</label>
